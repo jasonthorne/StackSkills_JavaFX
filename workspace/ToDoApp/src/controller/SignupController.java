a76bceb654a5e60a98513b47cd7e6b7a0f1f3a40
jasonthorne <jasonthorne@live.ie>
2020-09-26 00:50:29 +0100

    signupController

--- workspace/ToDoApp/src/controller/SignupController.java
+++ workspace/ToDoApp/src/controller/SignupController.java
@@ -1,5 +1,50 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class SignupController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField signupFirstName;
+
+    @FXML
+    private JFXTextField signupLastName;
+
+    @FXML
+    private JFXTextField signupUsername;
+
+    @FXML
+    private JFXTextField signupLocation;
+
+    @FXML
+    private JFXCheckBox signupCheckBoxMale;
+
+    @FXML
+    private JFXCheckBox signupCheckBoxFemale;
+
+    @FXML
+    private JFXPasswordField signupPassword;
+
+    @FXML
+    private JFXButton signupSignUpBtn;
+
+    @FXML
+    void initialize() {
+    	signupSignUpBtn.setOnAction(event -> {
+    		System.out.println("'signupSignUpBtn' clicked"); 
+    	});
+
+    }
 }
