0518e25ea8e2e448f5528d744a6f038fb81a6ff6
jasonthorne <jasonthorne@live.ie>
2020-09-26 21:29:13 +0100

    BuildDB.sql

---
+++ workspace/ToDoApp/BuildDB.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS to_do_app_db;
+CREATE DATABASE to_do_app_db;
+USE to_do_app_db;
+
+CREATE TABLE users( 
+  userID int NOT NULL AUTO_INCREMENT,
+  first_name varchar(64) NOT NULL,
+  last_name varchar(64) NOT NULL,
+  username varchar(64) NOT NULL,
+  password varchar(64) NOT NULL,
+  location varchar(64) NOT NULL,
+  gender varchar(64) NOT NULL,
+  PRIMARY KEY (userID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE tasks( 
+  taskID int NOT NULL AUTO_INCREMENT,
+  userID int NOT NULL,
+  date_created datetime NOT NULL,
+  description varchar(64) NOT NULL,
+  PRIMARY KEY (taskID),
+  FOREIGN KEY (userID) REFERENCES users(userID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
