db6152d2fdb93d181ae5c2478c9074bb85263db0
jasonthorne <jasonthorne@live.ie>
2020-10-10 22:57:36 +0100

    navigating to updateForm

--- workspace/ToDoApp/src/controller/CellController.java
+++ workspace/ToDoApp/src/controller/CellController.java
@@ -9,10 +9,13 @@ import com.jfoenix.controls.JFXListCell;
 import database.DatabaseHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.Task;
 
 public class CellController extends JFXListCell<Task>{ /** +++++IMPORTANT (type of 'Task')+++++++++++++++++++ */
@@ -33,6 +36,8 @@ public class CellController extends JFXListCell<Task>{ /** +++++IMPORTANT (type 
     private Label cellDateLbl;
     @FXML
     private ImageView cellDeleteImgView;
+    @FXML
+    private ImageView cellRefreshImgView;
     
     //private FXMLLoader fxmlLoader; //==================????????????
     int count = 0;
@@ -61,36 +66,56 @@ public class CellController extends JFXListCell<Task>{ /** +++++IMPORTANT (type 
 	    	 //if no instantiated loader:
 	    	//if(fxmlLoader == null) { 
 	   
-	    		//load fxml tree, and set it's controller as this:
-    	   		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/cell.fxml")); 
-	    	 	//fxmlLoader = new FXMLLoader(getClass().getResource("/view/cell.fxml")); 
-	    		 fxmlLoader.setController(this); //??????????
-	    		 try {
-					fxmlLoader.load(); //load fxml tree
-				 } catch (IOException e) {e.printStackTrace();}
-		    		 
-	    		 //populate cell fields with data from task:
-	    		 cellTaskNameLbl.setText(task.getTask());
-	    		 cellDescriptionLbl.setText(task.getDescription());
-	    		 cellDateLbl.setText(task.getDateCreated().toString());
+			//load fxml tree, and set it's controller as this:
+	   		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/cell.fxml")); 
+		 	//fxmlLoader = new FXMLLoader(getClass().getResource("/view/cell.fxml")); 
+			 fxmlLoader.setController(this); //??????????
+			 try {
+				fxmlLoader.load(); //load fxml tree
+			 } catch (IOException e) {e.printStackTrace();}
 	    		 
-	    		 //------------------cellDeleteImgView:-------------------------------------------------------
-	    		 //give handler to delete btn (is an image view, so needs .setOnMouseClicked ):
-	    		 cellDeleteImgView.setOnMouseClicked(event -> {
-    	    		System.out.println("delete btn clicked");
-    	    		
-    	    		/** BEFORE REMOVAL - show pop up confirming removal, on yes, THEN remove ++++++++++++++++*/
-    	    		//'getListView' is from 'ListCell' which we're extending from, by extending from 'JFXListCell'
-    	    		//'getItem' is from Cell, parent of 'ListCell'
-    	    		getListView().getItems().remove(getItem());  //get items from listView and remove this current item. +++++++++
-	    	    
-    	    		//remove item from db:
-    	    		DatabaseHandler.deleteTask(task);
-    	    		
-	    		 });
-	    		 
-	    		 setText(null); //set text as null
-		         setGraphic(cellRoot); //set this anchorpane to be the graphic
+			 //populate cell fields with data from task:
+			 cellTaskNameLbl.setText(task.getTask());
+			 cellDescriptionLbl.setText(task.getDescription());
+			 cellDateLbl.setText(task.getDateCreated().toString());
+			 
+			 //------------------cellDeleteImgView:-------------------------------------------------------
+			 //give handler to delete btn (is an image view, so needs .setOnMouseClicked ):
+			 cellDeleteImgView.setOnMouseClicked(event -> {
+	    		System.out.println("delete btn clicked");
+	    		
+	    		/** BEFORE REMOVAL - show pop up confirming removal, on yes, THEN remove ++++++++++++++++*/
+	    		//'getListView' is from 'ListCell' which we're extending from, by extending from 'JFXListCell'
+	    		//'getItem' is from Cell, parent of 'ListCell'
+	    		getListView().getItems().remove(getItem());  //get items from listView and remove this current item. +++++++++
+		    
+	    		//remove item from db:
+	    		DatabaseHandler.deleteTask(task);
+			 });
+			 
+			 //------------------cellRefreshImgView:-------------------------------------------------------
+			 cellRefreshImgView.setOnMouseClicked(event -> {
+				 
+				//load fxml tree from updateTaskForm.fxml
+			   	FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/updateTaskForm.fxml")); 
+			   	try {
+					loader.load(); //load fxml tree
+				} catch (IOException e) { e.printStackTrace(); }
+			   	
+			   	Parent root = loader.getRoot(); //create root from the loader's root (an anchorPane in this case)
+				Stage signUpStage = new Stage(); /**make a new stage for showing updateTask window */
+				
+				//add the root to a new scene. Add that scene to the stage:
+				signUpStage.setScene(new Scene(root));
+				signUpStage.showAndWait(); /** show the new stage */
+			   	
+			 });
+			 
+			 
+			 //--------------------------------------------------------------------------------------------
+			 
+			 setText(null); //set text as null
+	         setGraphic(cellRoot); //set this anchorpane to be the graphic
 	    	//} 
 	     }
 	 }
