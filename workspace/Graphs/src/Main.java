914b70ca955e1538adda44786c2da81b30c4c6b6
jasonthorne <jasonthorne@live.ie>
2021-07-27 21:02:31 +0100

    graphs testing - pie chart test workling

--- workspace/Graphs/src/Main.java
+++ workspace/Graphs/src/Main.java
@@ -7,45 +7,50 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 
 public class Main extends Application {
 	
 	//-------------------------------BAR CHART---------------------------
 
+	
+	/*
 	final static String raf = "RAF";
     final static String luftwaffe = "Luftwaffe";
     final static String russia = "Russia";
-    //final static String italy = "Italy";*/
-    //final static String usa = "USA";
  
     @Override public void start(Stage stage) {
         stage.setTitle("Bar Chart Sample");
         final CategoryAxis xAxis = new CategoryAxis();
         
-       /* xAxis.setCategories(FXCollections.<String>observableArrayList(
-                Arrays.asList(raf, luftwaffe, russia)));*/
-        
+       
         /////////https://stackoverflow.com/questions/55301550/javafx-barchart-how-to-center-align-bars ===========
+        
         
         final NumberAxis yAxis = new NumberAxis();
         final BarChart<String,Number> bc = 
@@ -53,65 +58,100 @@ public class Main extends Application {
         bc.setTitle("Country Summary");
         xAxis.setLabel("Country");       
         yAxis.setLabel("Value");
+        
+        //bc.setLegendSide(Side.LEFT);
+        
  
         XYChart.Series<String,Number> series1 = new XYChart.Series<String, Number>();
         series1.setName("spitfire");    
         series1.getData().add(new XYChart.Data(raf, 25601.34));
-        /*series1.getData().add(new XYChart.Data("Hurricane", 20148.82));
-        series1.getData().add(new XYChart.Data("raf plane 3", 20148.82));
-        series1.getData().add(new XYChart.Data("raf plane 4", 20148.82));
-        /*series1.getData().add(new XYChart.Data(france, 10000));
-        series1.getData().add(new XYChart.Data(italy, 35407.15));
-        series1.getData().add(new XYChart.Data(usa, 12000));      */
-        
+       
         bc.getData().add(series1);
         
         XYChart.Series<String,Number> series2 = new XYChart.Series<String, Number>();
         series2.setName("bf109");
         series2.getData().add(new XYChart.Data(luftwaffe, 57401.85));
-        /*series2.getData().add(new XYChart.Data("russia plane 2", 20148.82));
-        series2.getData().add(new XYChart.Data("russia plane 3", 20148.82));
-        series2.getData().add(new XYChart.Data("russia plane 4", 20148.82));*/
-        /*series2.getData().add(new XYChart.Data(brazil, 41941.19));
-        series2.getData().add(new XYChart.Data(france, 45263.37));
-        series2.getData().add(new XYChart.Data(italy, 117320.16));
-        series2.getData().add(new XYChart.Data(usa, 14845.27));  */
+       //series2.getData().add(new XYChart.Data("russia plane 2", 20148.82));
+       
         
         bc.getData().add(series2);
         
         XYChart.Series<String,Number> series3 = new XYChart.Series<String, Number>();
         series3.setName("Hurricane");
-        /*series3.getData().add(new Data<String, Number>(raf, 45000.65));
-        series3.getData().add(new XYChart.Data<String, Number>(russia, 45000.65));*/
-        
-        series3.getData().addAll(Arrays.asList(new Data<String, Number>(raf, 45000.65), new XYChart.Data<String, Number>(russia, 45000.65)));
         
         
-       /* series3.getData().add(new XYChart.Data(brazil, 44835.76));
-        series3.getData().add(new XYChart.Data(france, 18722.18));
-        series3.getData().add(new XYChart.Data(italy, 17557.31));*/
-        //series3.getData().add(new XYChart.Data(usa, 92633.68));  
+        series3.getData().addAll(
+        		Arrays.asList(new Data<String, Number>(raf, 45000.65), new XYChart.Data<String, Number>(russia, 45000.65)));
         
         bc.getData().add(series3);
         
-        /*
-        XYChart.Series<String,Number> series4 = new XYChart.Series<String, Number>();
-        series4.setName("Hurricane");
-        series4.getData().add(new XYChart.Data(russia, 45000.65));
-     
-        
-        bc.getData().add(series4);*/
         
         Scene scene  = new Scene(bc,800,600);
         
-       
         stage.setScene(scene);
         stage.show();
         
-        //----------------------------------------------------------
+       
         
-        
-    }
+    } */
+	
+	 //---------------------------------------------------------
+	
+	
+	//Pie chart:
+	
+	 @Override public void start(Stage stage) {
+	        Scene scene = new Scene(new Group());
+	        stage.setTitle("Imported Fruits");
+	        stage.setWidth(500);
+	        stage.setHeight(500);
+	 
+	        ObservableList<PieChart.Data> pieChartData =
+	                FXCollections.observableArrayList(
+	                new PieChart.Data("Grapefruit", 13),
+	                new PieChart.Data("Oranges", 25),
+	                new PieChart.Data("Plums", 10),
+	                new PieChart.Data("Pears", 22),
+	                new PieChart.Data("Apples", 30));
+	                		
+	        final PieChart chart = new PieChart(pieChartData);
+	        
+	        //chart.setLegendVisible(false);
+	        
+	        //=====================================CLICK EVENT
+	        final Label caption = new Label("");
+	        caption.setTextFill(Color.WHITE);
+	        caption.setStyle("-fx-font: 24 arial;");
+
+	        for (final PieChart.Data data : chart.getData()) {
+	            data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
+	                new EventHandler<MouseEvent>() {
+	                    @Override public void handle(MouseEvent e) {
+	                        caption.setTranslateX(e.getSceneX());
+	                        caption.setTranslateY(e.getSceneY());
+	                        ///caption.setText(String.valueOf(data.getPieValue()) + "%");
+	                        caption.setText(String.valueOf(data.getName()));
+	                        
+	                        System.out.println("yo");
+	                     }
+	                });
+	        }
+	        //====================================
+	        
+	        chart.setTitle("Imported Fruits");
+
+	        ((Group) scene.getRoot()).getChildren().addAll(chart, caption);
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		launch(args);
